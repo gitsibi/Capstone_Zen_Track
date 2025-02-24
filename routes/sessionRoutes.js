4afcd5ae73b2d0cd5700891eced385718d15185c
Jananisibi <jananisibi2002@gmail.com>
2025-02-24 22:22:33 +0530

    Added GET api endpoint for fetchig sessions

---
+++ routes/sessionRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const Session = require('../models/sessionModel');
+router.get('/', async (req, res) => {
+    try {
+        const sessions = await Session.find();
+        res.json(sessions);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+module.exports = router;
